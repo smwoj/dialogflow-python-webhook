3cc33782536d09bdcef617906e79fd14d2f3656a
Wojciech Smołka <sm.wojciech@gmail.com>
2018-11-04 00:12:33 +0100

    another checkpoint

--- api_test.py
+++ api_test.py
@@ -27,3 +27,34 @@ if __name__ == '__main__':
             requests.post(ENDPOINT, json=_bigass_json).content.decode()
         # )
     )
+
+
+"""
+ 2003  sudo docker build -t df-app:minimal
+ 2004  sudo docker build -t df-app:minimal .
+ 2005  sudo docker run 
+ 2006  sudo docker run -p 8080 -d -n df-app  df-app:minimal
+ 2007  sudo docker run -p 8080 -d --name df-app  df-app:minimal
+ 2008  sudo docker container ls -a
+ 2009  sudo docker stop df-app
+ 2010  sudo docker rm df-app
+ 2011  sudo docker rm vibrant-liskov
+ 2012  sudo docker container ls -a
+ 2013  sudo docker rm vibrant_liskov
+ 2014  sudo docker container ls -a
+ 2015  clear
+ 2016  sudo docker run -p 8080:8080 -d --name df-app  df-app:minimal
+ 2017  gcloud
+ 2018  gcloud project
+ 2019  gcloud projects
+ 2020  gcloud projects --help
+ 2021  gcloud projects set
+ 2022  gcloud project
+ 2023  gcloud set project
+ 2024  gcloud project
+ 2025  gcloud auth configure-docker
+ 2026  gcloud components update
+ 2027  gcloud config set project hackathon-webhooks
+ 2028  history
+
+"""
--- main.py
+++ main.py
@@ -25,14 +25,14 @@ def start(api_endpoint):
     @app.route(f'/api/{api_endpoint}', methods=['POST'])
     def _api_dispatcher():
         request_data = request.get_json()
-        # request_data = json.loads(request_data)
-
-        pprint.pprint(request_data)
+        if isinstance(request_data, str):
+            request_data = json.loads(request_data)
 
         intent = request_data['queryResult']['intent']['displayName']
         params = request_data['queryResult']['parameters']
 
         msg = intent_handlers[intent](intent, **params)
+
 
         from dialogflow_spec import make_response
         return make_response(
@@ -40,10 +40,15 @@ def start(api_endpoint):
             tts_message=msg,
         )
 
-    app.run(debug=True, host=const.HOST, port=const.PORT)
+    app.run(debug=True, host=const.HOST, port=const.PORT
+            # , ssl_context='adhoc'
+            )
 
 
 if "__main__" == __name__:
     assert os.getcwd() == const.ROOT, f"The server must be run from repo root dir: {const.ROOT}"
 
     start(api_endpoint=sys.argv[1])
+"""
+curl -H "Content-Type: application/json; charset=utf-8"  -H "Authorization: Bearer ya29.c.ElpJBjPI4X5IsxXp6ENtjAjcWCI4PhouvrKhhAAwrMsfDU8XqPQvBL062CGjEkkn8OUqoWrQmRIGoiwTOj2OyjIqjuQmd1eKS6Upo5RRwgFD1F3H2X-tbZ3eX8o"  -d "{\"queryInput\":{\"text\":{\"text\":\"tell me  a dirty joke\",\"languageCode\":\"en\"}},\"queryParams\":{\"timeZone\":\"Europe/Warsaw\"}}" "https://dialogflow.googleapis.com/v2/projects/hackathon-app-220820/agent/sessions/c937dbf8-d227-4eba-5dd9-36508dcd6cc5:detectIntent"
+"""
